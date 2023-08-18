8dcc7ac8a537416c6a19fa5d6a2f97d3ec68200e
Noah-Bunis <141171556+Noah-Bunis@users.noreply.github.com>
2023-08-17 22:29:38 -0400

    Project Files
    0.0.2

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,13 +1,10 @@
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
+public class CameraMovement : MonoBehaviour
 {
     [SerializeField] public Transform player;            // Reference to the player's transform
     public float followRadius = 5.0f;   // The desired radius between camera and player
-    public float followOffsetX = 0.0f;
-    public float followOffsetY = 0.0f;
     public float followSpeed = 5.0f;    // The speed of camera movement
-    public bool isCameraPanning = false;
 
     private void Update()
     {
@@ -22,28 +19,9 @@ public class CameraFollow : MonoBehaviour
         Vector3 cameraToPlayer = playerPosition - transform.position;
 
         // Calculate the desired camera position within the follow radius
-        Vector3 desiredPosition = playerPosition - cameraToPlayer.normalized * followRadius + new Vector3(followOffsetX, followOffsetY, 0.0f);
+        Vector3 desiredPosition = playerPosition - cameraToPlayer.normalized * followRadius;
 
-        CheckCameraPanning(cameraToPlayer, desiredPosition);
-        if (isCameraPanning) PanCamera(cameraToPlayer, desiredPosition);
-    }
-
-    private void CheckCameraPanning(Vector3 cameraToPlayer, Vector3 desiredPosition)
-    {
-        if (Mathf.Abs(cameraToPlayer.x) > followRadius || Mathf.Abs(cameraToPlayer.y) > followRadius)
-        {
-            isCameraPanning = true;
-        }
-    }
-
-    private void PanCamera(Vector3 cameraToPlayer, Vector3 desiredPosition)
-    {
         // Interpolate the camera's position towards the desired position
         transform.position = Vector3.Lerp(transform.position, desiredPosition, followSpeed * Time.deltaTime);
-
-        if (Mathf.Abs(cameraToPlayer.x) < 1f && Mathf.Abs(cameraToPlayer.y) < 1f) 
-        {
-            isCameraPanning = false;
-        }
     }
 }
---
+++ Assets/Scripts/TerrainTile.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainTile : MonoBehaviour
+{
+    [SerializeField] Vector2Int tilePosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponentInParent<WorldScrolling>().Add(gameObject, tilePosition);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
